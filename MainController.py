d06e6b09455e4f70c4a923c56307f0828ec9f9cb
unknown <h2hallowdy@gmail.com>
2019-04-15 19:56:36 +0700

    get home position for camera

--- MainController.py
+++ MainController.py
@@ -11,8 +11,10 @@ import time
 import serial
 import array
 import logging
+import cv2
 from datetime import datetime
 from ObjectDetection import ObjectDetection
+from Camera import Camera
 
 # logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
 # logging.debug('This message should go to the log file')
@@ -24,6 +26,7 @@ class Ui_MainControllerUI(object):
     stateLiveView = False
     stateAutoMode = False
     stateProcess = False
+    stateHoming = False
     # running = threading.Event()
     # running.set()
     ser = serial.Serial()
@@ -31,6 +34,10 @@ class Ui_MainControllerUI(object):
     # ser.port = "COM1"
     def __init__(self, camera=None):
         super(Ui_MainControllerUI, self).__init__()
+        self.homeX = 0
+        self.homeY = 0
+        self.camera = Camera(0)
+        
         self.od = ObjectDetection()
         t_filename = GetTimeForFile()
         self.FILE_LOG = 'logs/' + t_filename + '.log'
@@ -331,6 +338,7 @@ class Ui_MainControllerUI(object):
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.read_data)
         self.timer.start(300)
+        
         # Initialize callbacks and events
         ## For Screens
         self.teachingCamBtn.clicked.connect(self.openCamTeaching)
@@ -342,6 +350,7 @@ class Ui_MainControllerUI(object):
         self.armTestingBtn.clicked.connect(self.enableCam)
         self.manualBtn.clicked.connect(self.manualMode)
         self.autoBtn.clicked.connect(self.autoMode)
+        self.camHomeBtn.clicked.connect(self.CamHoming)
         
 
 
@@ -533,6 +542,7 @@ class Ui_MainControllerUI(object):
             self.updateTimer = QtCore.QTimer()
             self.updateTimer.timeout.connect(self.update_Image)
             self.updateTimer.start(1)
+            
         else:
             logging.basicConfig(filename=self.FILE_LOG, level=logging.INFO)
             t_log = GetTime()
@@ -632,7 +642,59 @@ class Ui_MainControllerUI(object):
         else:
             logging.basicConfig(filename=self.FILE_LOG, level=logging.INFO)
             t_log = GetTime()
-            logging.info(t_log + ': Exit auto Mode.')       
+            logging.info(t_log + ': Exit auto Mode.')  
+
+    ########################################################################################
+    #                                                                                      #
+    # Camera Homing                                                                        #
+    #                                                                                      #
+    ########################################################################################
+    def CamHoming(self):
+        self.stateHoming = not self.stateHoming
+        if self.stateHoming == True:
+            # for homing
+            self.camera.initialize()
+            self.updateTimerHoming = QtCore.QTimer()
+            self.updateTimerHoming.timeout.connect(self.ImageForHoming)
+            self.updateTimerHoming.start(1)
+            
+        else:
+            last_frame = self.camera.get_frame()
+            cv2.imwrite('LastFrame.jpg', last_frame)
+            self.GetHome(last_frame)
+            self.camera.close_camera()
+            self.updateTimerHoming.stop()
+    def ImageForHoming(self):
+        frame = self.camera.get_frame()
+        height, width, channel = frame.shape
+        bytesPerLine = 3 * width
+        qImg = QtGui.QImage(frame.data, width, height, bytesPerLine, QtGui.QImage.Format_RGB888).rgbSwapped()
+        qPixMap = QtGui.QPixmap(qImg)
+        qPixMap = qPixMap.scaled(self.liveVidFrame.width(), self.liveVidFrame.height(),QtCore.Qt.KeepAspectRatio)
+        self.liveVidFrame.setPixmap(qPixMap)
+        self.updateTimerHoming.setInterval(4)   
+    
+    def GetHome(self, img):
+        img = cv2.resize(img, (640, 480))
+
+        cg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        median = cv2.medianBlur(cg, 5)
+        th = cv2.adaptiveThreshold(median, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 13, 2)
+
+        circles = cv2.HoughCircles(th, cv2.HOUGH_GRADIENT, 1, 20,
+                               param1=30, param2=20,
+                               minRadius=20, maxRadius=30)
+        if circles is not None:
+            circles = np.uint16(np.around(circles))
+            for i in circles[0, :]:
+                center = (i[0], i[1])      
+                cv2.circle(median, center, 1, (0, 100, 100), 2)
+                # circle outline
+                radius = i[2]
+                cv2.circle(median, center, radius, (255, 0, 0), 2)
+                cv2.putText(median, str(center[1]), (center[0] + 10, center[1] + 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)    
+        print(circles)
 
 if __name__ == "__main__":
     import sys
